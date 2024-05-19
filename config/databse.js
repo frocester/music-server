ec8044dfd08fd727edc843353dbc657ad4a25bbb
frocester <frocestersam2@gmail.com>
2024-05-20 02:27:22 +0530

    error fix

--- config/databse.js
+++ config/databse.js
@@ -4,15 +4,15 @@ require("dotenv").config();
 const dbConnect = () => {
   mongoose
     .connect(process.env.DATABASE_URL, {
-      useNewurlParser: true,
-      useUnifiedTopology: true,
+      // useNewurlParser: true,
+      // useUnifiedTopology: true,
     })
     .then(() => {
       console.log("DB connection successful");
     })
     .catch((err) => {
       console.log(err);
-      console.error(error.message);
+      // console.error(error.message);
     });
 };
 
